2d69cd8c21198c40eca5766eb9f5ba78375f6e81
Your Name <you@example.com>
2018-10-24 20:47:17 -0300

    Home

--- src/_modules/header/header.js
+++ src/_modules/header/header.js
@@ -3,19 +3,22 @@
 // Constructor
 var Header = function() {
     var header = $('.header');
-    var body = $('body');
+    var home = $('home');
+    var homeHero = $('.home__hero');
     var menuOpen = $('.header__hamburguer');
-    var menuClose = $('.header__nav__close');
+    // var menuClose = $('.header__nav__close');
 
     menuOpen.on('click', function(){
-        header.addClass('-open');
-        body.addClass('-hideOverflow');
+        header.toggleClass('-open');
+        homeHero.toggleClass('-open');
+        home.toggleClass('-hideOverflow');
     });
 
-    menuClose.on('click', function(){
-        header.removeClass('-open');
-        body.removeClass('-hideOverflow');
-    });
+    //
+    // menuClose.on('click', function(){
+    //     header.removeClass('-open');
+    //     body.removeClass('-hideOverflow');
+    // });
 };
 
 module.exports = Header;
