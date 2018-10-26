d2cd4740ad1d532a381ca40ae19eae780d41dd71
Your Name <you@example.com>
2018-10-26 10:12:46 -0300

    Home Update

--- src/_modules/header/header.js
+++ src/_modules/header/header.js
@@ -4,21 +4,14 @@
 var Header = function() {
     var header = $('.header');
     var home = $('home');
-    var homeHero = $('.home__hero');
+    var hero = $('.js-hero');
     var menuOpen = $('.header__hamburguer');
-    // var menuClose = $('.header__nav__close');
 
     menuOpen.on('click', function(){
         header.toggleClass('-open');
-        homeHero.toggleClass('-open');
+        hero.toggleClass('-open');
         home.toggleClass('-hideOverflow');
     });
-
-    //
-    // menuClose.on('click', function(){
-    //     header.removeClass('-open');
-    //     body.removeClass('-hideOverflow');
-    // });
 };
 
 module.exports = Header;
